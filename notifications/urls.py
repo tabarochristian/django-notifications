4c5357102918408da4fe17c30858a6966d87d9d2
Tabaro Christian <christian@tabaro.me>
2025-09-14 21:02:02 +0100

    Update urls.py

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,6 +1,9 @@
 ''' Django notification urls file '''
 # -*- coding: utf-8 -*-
-from distutils.version import StrictVersion  # pylint: disable=no-name-in-module,import-error
+# from distutils.version import StrictVersion  # pylint: disable=no-name-in-module,import-error
+
+from packaging.version import \
+    Version as StrictVersion
 
 from django import get_version
 
