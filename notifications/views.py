6de23535a7ecbc3e9c4cc5df0f8b15455f692b9d
Tabaro Christian <christian@tabaro.me>
2025-09-14 21:02:25 +0100

    Update views.py

--- notifications/views.py
+++ notifications/views.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 ''' Django Notifications example views '''
-from distutils.version import \
-    StrictVersion  # pylint: disable=no-name-in-module,import-error
+# from distutils.version import \
+#     StrictVersion  # pylint: disable=no-name-in-module,import-error
+
+from packaging.version import \
+    Version as StrictVersion
 
 from django import get_version
 from django.conf import settings
